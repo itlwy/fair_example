f21762bdf952ab12368a9f7ab144aab8ab67f809
李伟业 <weiyeli@sfmail.sf-express.com>
2021-03-12 14:52:48 +0800

    依赖修改

--- lib/src/pages/form/form_delegate.dart
+++ lib/src/pages/form/form_delegate.dart
@@ -1,3 +1,8 @@
+import 'dart:convert';
+
+import 'package:crypto/crypto.dart';
+import 'package:convert/convert.dart';
+
 import 'package:fair/fair.dart';
 import 'package:fair_example/src/utils/form/form_builder.dart';
 import 'package:flutter/material.dart';
@@ -38,7 +43,18 @@ class FormDelegate extends FairDelegate {
   void onSubmit() {
     formKey.currentState.save();
     if (formKey.currentState.validate()) {
+      // calculateMD5();
       print('Processing Data ${formKey.currentState.value.toString()}');
+    }
+  }
+
+  void calculateMD5() {
+    String content = "What a fine day, I can do a lot of funny stuffddd.";
+    for (int i = 0; i < 1000; i++) {
+      List<int> contentConvert = utf8.encode(content);
+      var digest = md5.convert(contentConvert);
+      var result = hex.encode(digest.bytes);
+      print("md5 result: $result");
     }
   }
 }
